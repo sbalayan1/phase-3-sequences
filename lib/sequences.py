411cfc171cff89d82605aad780fd3d0a57f32a84
sbalayan1 <balayans2014@yahoo.com>
2022-11-20 00:08:51 -0800

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    res = []
+    if (length > 0): res.append(0)
+    if (length > 1): res.append(1)
+    for i in range(2,length):        
+        res.append(res[i-1] + res[i-2])
+
+    print(res)
+
+# def recursion(length):
+#     if (length == 0): return 0
+#     if (length == 1): return 1
+
+#     return recursion(length-1) + recursion(length - 2)
